8439190c3f7751f67c0caf7beba0ecf22ca57d2e
Tim Weidner <timaa2k@gmail.com>
2020-03-21 23:19:11 +0100

    feat(bash): fzf completions + keybindings

--- setup/common/shells.nix
+++ setup/common/shells.nix
@@ -3,5 +3,6 @@
 {
   environment.shells = [ pkgs.bash ];
   programs.bash.enable = true;
+  programs.bash.enableCompletion = true;
   environment.etc."bash.local".text = builtins.readFile ../../homies/bash/bashrc;
 }
