085069a5fedf9c5e1f061960c740a379aa97e295
Tim Weidner <timaa2k@gmail.com>
2020-03-22 01:16:37 +0100

    chore(bash): add bash.local sourcing comment

--- setup/common/shells.nix
+++ setup/common/shells.nix
@@ -4,5 +4,8 @@
   environment.shells = [ pkgs.bash ];
   programs.bash.enable = true;
   programs.bash.enableCompletion = true;
+
+  # NOTE(timaa2k): nix-darwin loads custom bashrc from this location.
+  # https://github.com/LnL7/nix-darwin/blob/1ffae69c561c67087f5023aa8b6b3dcc6078b04d/modules/programs/bash/default.nix#L97
   environment.etc."bash.local".text = builtins.readFile ../../homies/bash/bashrc;
 }
