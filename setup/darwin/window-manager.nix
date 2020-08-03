16463d8114a6f244ab888de2f9f391dec910498f
Tim Weidner <timaa2k@gmail.com>
2020-08-03 21:55:46 +0200

    separate yabai menubar into spacebar

--- setup/darwin/window-manager.nix
+++ setup/darwin/window-manager.nix
@@ -1,49 +1,58 @@
 {config, pkgs, ...}:
 
 {
+  services.spacebar.enable = true;
+  services.spacebar.package = pkgs.spacebar;
+  services.spacebar.config = {
+    position         = "top";
+    height           = 26;
+    text_font        = ''"Helvetica Neue:Bold:12.0"'';
+    icon_font        = ''"FontAwesome:Regular:13.0"'';
+    background_color = "0xff202020";
+    foreground_color = "0xffa8a8a8";
+    space_icon_strip = "I II III IV V VI VII VIII IX X";
+    power_icon_strip = " ";
+    space_icon       = "";
+    clock_icon       = "";
+    dnd_icon         = "";
+    clock_format     = ''"%R"'';
+  };
+
   services.yabai.enable = true;
   services.yabai.package = pkgs.yabai;
   services.yabai.enableScriptingAddition = true;
-  services.yabai.extraConfig = ''
-    yabai -m config status_bar                   on
-    yabai -m config status_bar_text_font         "Helvetica Neue:Bold:12.0"
-    yabai -m config status_bar_icon_font         "FontAwesome:Regular:13.0"
-    yabai -m config status_bar_background_color  0xff202020
-    yabai -m config status_bar_foreground_color  0xffa8a8a8
-    yabai -m config status_bar_space_icon_strip  I II III IV V VI VII VIII IX X
-    yabai -m config status_bar_power_icon_strip   
-    yabai -m config status_bar_space_icon        
-    yabai -m config status_bar_clock_icon        
+  services.yabai.config = {
+    external_bar                 = "all:26:0";
 
-    yabai -m config mouse_follows_focus          off
-    yabai -m config focus_follows_mouse          on
-    yabai -m config window_placement             second_child
-    yabai -m config window_topmost               off
-    yabai -m config window_opacity               off
-    yabai -m config window_opacity_duration      0.0
-    yabai -m config window_shadow                off
+    mouse_follows_focus          = "off";
+    focus_follows_mouse          = "on";
+    window_placement             = "second_child";
+    window_topmost               = "off";
+    window_opacity               = "off";
+    window_opacity_duration      = 0.0;
+    window_shadow                = "off";
 
-    yabai -m config window_border                on
-    yabai -m config window_border_placement      inset
-    yabai -m config window_border_width          1
-    yabai -m config window_border_radius         -1.0
-    yabai -m config active_window_border_topmost off
-    yabai -m config active_window_border_color   0xff775759
-    yabai -m config normal_window_border_color   0xff505050
-    yabai -m config insert_window_border_color   0xffd75f5ff
+    window_border                = "on";
+    window_border_placement      = "inset";
+    window_border_width          = "1";
+    window_border_radius         = -1.0;
+    active_window_border_topmost = "off";
+    active_window_border_color   = "0xff775759";
+    normal_window_border_color   = "0xff505050";
+    insert_window_border_color   = "0xffd75f5f";
 
-    yabai -m config active_window_opacity        1.0
-    yabai -m config normal_window_opacity        0.90
-    yabai -m config split_ratio                  0.5
-    yabai -m config auto_balance                 on
+    active_window_opacity        = 1.0;
+    normal_window_opacity        = 0.90;
+    split_ratio                  = 0.5;
+    auto_balance                 = "on";
 
-    yabai -m config layout                       bsp
-    yabai -m config top_padding                  0
-    yabai -m config bottom_padding               0
-    yabai -m config left_padding                 0
-    yabai -m config right_padding                0
-    yabai -m config window_gap                   0
-  '';
+    layout                       = "bsp";
+    top_padding                  = 0;
+    bottom_padding               = 0;
+    left_padding                 = 0;
+    right_padding                = 0;
+    window_gap                   = 0;
+  };
 
   services.skhd.enable = true;
   services.skhd.package =  pkgs.skhd;
