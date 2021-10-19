133ed8bca1013a67604565eb0ca634173885153d
Tim Weidner <timaa2k@gmail.com>
2021-10-19 21:16:09 +0200

    udpate yabai

--- overlays/yabai/default.nix
+++ overlays/yabai/default.nix
@@ -2,12 +2,12 @@ self: super:
 {
   yabai = super.yabai.overrideAttrs (old: rec {
     pname = "yabai";
-    version = "3.2.1";
+    version = "3.3.10";
     src = self.fetchFromGitHub {
       owner = "koekeishiya";
       repo = pname;
       rev = "v${version}";
-      sha256 = "11rsi6z2z7ynfqs1xq3bvf187k5xnwm0d45a8ai9hrqdsf3f1j19";
+      sha256 = "1gd88s3a05qvvyjhk5wpw1crb7p1gik1gdxn7pv2vq1x7zyvzvph";
     };
   });
 }
