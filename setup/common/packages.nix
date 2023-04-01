8cd22feea478e32862d9c2a21631eb3d0c214fb3
Tim Weidner <timaa2k@gmail.com>
2023-04-01 20:21:33 +0200

    updated and refined configuration

--- setup/common/packages.nix
+++ setup/common/packages.nix
@@ -8,10 +8,9 @@ let
       peel = mkCache "https://peel.cachix.org" "peel.cachix.org-1:juIxrHgL76bYKcfIB/AdBUQuwkTwW5OLpPvWNuzhNrE=";
       cachix = mkCache "https://cachix.cachix.org" "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM=";
       hercules-ci = mkCache "https://hercules-ci.cachix.org" "hercules-ci.cachix.org-1:ZZeDl9Va+xe9j+KqdzoBZMFJHVQ42Uu/c/1/KMC5Lw0=";
-      hie-nix = mkCache "https://hie-nix.cachix.org" "hie-nix.cachix.org-1:EjBSHzF6VmDnzqlldGXbi0RM3HdjfTU3yDRi9Pd0jTY=";
       all-hies = mkCache "https://all-hies.cachix.org" "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k=";
       nix-tools = mkCache "https://nix-tools.cachix.org" "nix-tools.cachix.org-1:ebBEBZLogLxcCvipq2MTvuHlP7ZRdkazFSQsbs0Px1A=";
-    in [ nixos peel cachix hercules-ci hie-nix all-hies nix-tools ];
+    in [ nixos peel cachix hercules-ci all-hies nix-tools ];
 in {
   nix.binaryCaches = builtins.map (x: x.url) caches;
   nix.binaryCachePublicKeys = builtins.map (x: x.key) caches;
@@ -32,52 +31,39 @@ in {
   nixpkgs.config.allowUnfreeRedistributable = true;
 
   environment.systemPackages = with pkgs; [
-    bat
-    # NOTE(tweidner): Broken
-    # cdrtools
+    bash-completion
+    bash-configured
     coreutils
-    fd
+    curl
+    exa
     findutils
     fzf
-    gnumake
+    git-configured
     gnupg
+    go
+    gopls
+    gotools
     htop
     jq
+    less
+    ncdu
+    ncurses
+    neovim-configured
+    nix
     nix-prefetch-scripts
-    ps
-    pass
-    ripgrep
-    tmuxp
-    tree
-    unzip
-    youtube-dl
-
-    dot-git
-    dot-lf
-    dot-tmux
-    dot-neovim
-    (python3.withPackages (p: with p; [
-    ]))
     nodejs
-    nodePackages."@vue/cli"
-    #niv
-    go
-    godef
-    gotools
-    gopls
+    ps
+    python3
+    python3Packages.pynvim
+    ripgrep
+    tree
+    tmux-configured
+    tmuxp
+    unzip
+    xclip
+    youtube-dl
+    ###
     alacritty
     #kitty
-    bash-completion
-    # cachix
-    cmake
-    direnv
-    exa
-
-    docker-compose
-
-    kind
-    s3fs
-    vscode-with-extensions
-    yarn
   ];
 }
